0eef097783f9a89b8b417753350e4f102119402d
Michael Schneider <mic.sch91@gmail.com>
2020-06-01 19:59:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,10 +5,16 @@ def loop_message_five_times(message)
 end
   end
 
+
 def output_array(array)
   counter = 0
 while counter < array.length do
   puts array[counter]
   counter += 1
 end
+  end
+
+def return_string_array(array)
+  array.to_s
+return array.to_s
 end
