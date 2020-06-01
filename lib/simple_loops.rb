5c1da864aa48b955d612ef53bb835100414173a6
Michael Schneider <mic.sch91@gmail.com>
2020-06-01 19:45:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,3 +4,11 @@ def loop_message_five_times(message)
   puts "Hello World."
 end
   end
+
+def loop_message_n_times(message, number)
+  counter = 0
+while message[counter] do
+  puts message[counter]
+  counter += 1
+end
+  end
